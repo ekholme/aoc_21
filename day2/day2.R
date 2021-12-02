ff77df63ce9729a97f4445a2cbb82f94c3902d24
Eric Ekholm <eric.ekholm@gmail.com>
2021-12-02 11:14:23 -0500

    day 2 part 2 r

--- day2/day2.R
+++ day2/day2.R
@@ -12,4 +12,27 @@ names(vals) <- nms
 
 y <- sum(vals[names(vals) == "forward"]) * (sum(vals[names(vals) == "down"]) - sum(vals[names(vals) == "up"]))
 
+##PART 2
+library(tidyverse)
 
+#sum of forward will remain the same
+f <- sum(vals[names(vals) == "forward"])
+
+df <- tibble(
+  cmd = names(vals),
+  val = vals
+) %>%
+  mutate(aim_delta = case_when(
+    cmd == "forward" ~ 0,
+    cmd == "down" ~ vals,
+    cmd == "up" ~ -1*vals
+  ),
+  aim_cur = cumsum(aim_delta))
+
+depth <- df %>% 
+  filter(cmd == "forward") %>%
+  mutate(depth = aim_cur*val) %>%
+  summarize(sum(depth)) %>%
+  pull()
+
+f*depth
