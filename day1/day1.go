bd04f40e58e136987652e8df9bf08507e97f1dcd
Eric Ekholm <eric.ekholm@gmail.com>
2022-07-06 10:31:11 -0400

    add go day1 pt2

--- day1/day1.go
+++ day1/day1.go
@@ -22,25 +22,65 @@ func readData() ([][]string, error) {
 	return lines, err
 }
 
+func convertStrings(s [][]string) []int {
+	var js []int
+
+	for i := 0; i < len(s); i++ {
+		a := strings.Join(s[i], "")
+		j, err := strconv.Atoi(a)
+
+		if err != nil {
+			log.Fatal("Failed to convert strings to ints")
+		}
+
+		js = append(js, j)
+
+	}
+
+	return js
+}
+
 func main() {
+
+	//setting up data
 
 	lines, err := readData()
 	if err != nil {
 		log.Fatal("Failed to read csv")
 	}
 
-	d := strings.Join(lines[0], "")
-	depth, _ := strconv.Atoi(d)
+	data := convertStrings(lines)
+
+	//part 1
+
+	d := data[0]
 	counter := 0
 
-	for i := 1; i < len(lines); i++ {
-		a := strings.Join(lines[i], "")
-		b, _ := strconv.Atoi(a)
+	for i := 1; i < len(data); i++ {
+		a := data[i]
 
-		if b > depth {
+		if a > d {
 			counter++
 		}
-		depth = b
+		d = a
+	}
+
+	fmt.Println(counter)
+
+	//part 2
+
+	baseline := data[0] + data[1] + data[2]
+
+	counter = 0
+
+	for i := 1; i < len(data)-2; i++ {
+		s := data[i] + data[i+1] + data[i+2]
+
+		if s > baseline {
+			counter++
+		}
+
+		baseline = s
 	}
 
 	fmt.Println(counter)
