78a155aff38f319a09161f7fab2e05a07e892fdf
Eric Ekholm <eric.ekholm@gmail.com>
2022-07-07 09:44:54 -0400

    day2 pt 2 go

---
+++ day2/day2.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readData() ([][]string, error) {
+	file, err := os.Open("day2/input.csv")
+	if err != nil {
+		log.Fatal("Failed to open csv")
+	}
+
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+
+	return lines, err
+}
+
+type movement struct {
+	direction string
+	value     int
+}
+
+func main() {
+	d, err := readData()
+
+	if err != nil {
+		log.Fatal("error reading csv")
+	}
+
+	var moves []*movement
+
+	for _, s := range d {
+
+		i := strings.Join(s, "")
+		j := i[:len(i)-2]
+
+		k := i[len(i)-1:]
+		v, _ := strconv.Atoi(k)
+
+		m := &movement{
+			direction: j,
+			value:     v,
+		}
+
+		moves = append(moves, m)
+
+	}
+
+	depth := 0
+	horz := 0
+
+	for _, v := range moves {
+
+		if v.direction == "forward" {
+			horz += v.value
+		} else if v.direction == "up" {
+			depth -= v.value
+		} else if v.direction == "down" {
+			depth += v.value
+		}
+	}
+
+	res := depth * horz
+
+	fmt.Println(res)
+
+}
